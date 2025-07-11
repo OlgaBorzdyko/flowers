284ff5ea72b0b8cc5d790d258c909a1d63d0026c
OlgaBorzdyko <helga.borzdyko@gmail.com>
2025-07-11 16:21:44 +0400

    Added more products to handlers

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -18,12 +18,13 @@ export const handlers = [
         { id: 1, productName: 'Товар 1' },
         { id: 2, productName: 'Товар 2' },
         { id: 3, productName: 'Товар 3' },
-        { id: 1, productName: 'Товар 4' },
-        { id: 2, productName: 'Товар 5' },
-        { id: 3, productName: 'Товар 6' },
-        { id: 1, productName: 'Товар 7' },
-        { id: 2, productName: 'Товар 8' },
-        { id: 3, productName: 'Товар 9' }
+        { id: 4, productName: 'Товар 4' },
+        { id: 5, productName: 'Товар 5' },
+        { id: 6, productName: 'Товар 6' },
+        { id: 7, productName: 'Товар 7' },
+        { id: 8, productName: 'Товар 8' },
+        { id: 9, productName: 'Товар 9' },
+        { id: 10, productName: 'Товар 10' }
       ]
     }
     return new Response(JSON.stringify(products), {
