9ed944ec03f96e850b33622cab98ab504854a310
OlgaBorzdyko <helga.borzdyko@gmail.com>
2025-07-30 16:07:00 +0400

    Added Add to cart button

---
+++ src/cart/AddToCartButton.tsx
@@ -0,0 +1,13 @@
+import { Box, Button, Typography } from '@mui/material'
+
+const AddToCartButton = () => {
+  return (
+    <Box>
+      <Button>
+        <Typography>Добавить в корзину</Typography>
+      </Button>
+    </Box>
+  )
+}
+
+export default AddToCartButton
