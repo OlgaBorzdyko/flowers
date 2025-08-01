45919721c8e1bbe30d4fb3074ca76bb3e68b9dc6
OlgaBorzdyko <helga.borzdyko@gmail.com>
2025-08-01 19:02:11 +0400

    Added add and decr items for cart

--- src/cart/CartPage.tsx
+++ src/cart/CartPage.tsx
@@ -1,9 +1,9 @@
-import { Box, Table, Typography } from '@mui/material'
+import { Box, Button, Table, Typography } from '@mui/material'
 
 import { useCart } from './useCart'
 
 const CartPage = () => {
-  const { items } = useCart()
+  const { items, addItem, decrementItem } = useCart()
   return (
     <Box>
       <Typography>Корзина</Typography>
@@ -11,6 +11,23 @@ const CartPage = () => {
         {items.map((item, index) => (
           <Table key={index}>
             <Typography>{item.name}</Typography>
+            <Box>
+              <Button
+                onClick={() => {
+                  decrementItem(item)
+                }}
+              >
+                -
+              </Button>
+              {item.quantity}
+              <Button
+                onClick={() => {
+                  addItem(item)
+                }}
+              >
+                +
+              </Button>
+            </Box>
           </Table>
         ))}
       </Box>
