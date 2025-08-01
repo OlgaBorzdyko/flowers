dce1c34611b7de3697f0bfeee8e759e72695940b
OlgaBorzdyko <helga.borzdyko@gmail.com>
2025-08-01 18:54:16 +0400

    preload for cart

--- App.tsx
+++ App.tsx
@@ -1,11 +1,18 @@
+import { useEffect } from 'react'
 import { BrowserRouter } from 'react-router-dom'
 import styled from 'styled-components'
 
 import AppRoutes from './routes'
+import { useCart } from './src/cart/useCart'
 import Footer from './src/Footer'
 import Header from './src/Header'
 
 const App = () => {
+  const { cookiesLoading } = useCart()
+
+  useEffect(() => {
+    cookiesLoading()
+  }, [cookiesLoading])
   return (
     <BrowserRouter>
       <Wrapper>
