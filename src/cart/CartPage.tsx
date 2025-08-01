08a21190d4ef3fd9d03b5a36e400d9116a77e058
OlgaBorzdyko <helga.borzdyko@gmail.com>
2025-08-01 18:07:00 +0400

    Added cart page

---
+++ src/cart/CartPage.tsx
@@ -0,0 +1,10 @@
+import { Box, Typography } from '@mui/material'
+
+const CartPage = () => {
+  return (
+    <Box>
+      <Typography>Корзина</Typography>
+    </Box>
+  )
+}
+export default CartPage
