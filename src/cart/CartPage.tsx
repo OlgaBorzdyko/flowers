e8f275342ae05aa7af9972eab9e3636abb0175f2
OlgaBorzdyko <helga.borzdyko@gmail.com>
2025-08-01 18:35:10 +0400

    Added table and items to cart page

--- src/cart/CartPage.tsx
+++ src/cart/CartPage.tsx
@@ -1,9 +1,19 @@
-import { Box, Typography } from '@mui/material'
+import { Box, Table, Typography } from '@mui/material'
+
+import { useCart } from './useCart'
 
 const CartPage = () => {
+  const { items } = useCart()
   return (
     <Box>
       <Typography>Корзина</Typography>
+      <Box>
+        {items.map((item, index) => (
+          <Table key={index}>
+            <Typography>{item.name}</Typography>
+          </Table>
+        ))}
+      </Box>
     </Box>
   )
 }
