76673f7f8e6049150b63b4d9ec2180bbdfe8a3d1
OlgaBorzdyko <helga.borzdyko@gmail.com>
2025-08-04 15:47:59 +0400

    hot fix

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -15,16 +15,16 @@ export const handlers = [
       totalCount: 30,
       limit: 8,
       products: [
-        { id: 1, productName: 'Товар 1', img: '/bouquet.jpeg' },
-        { id: 2, productName: 'Товар 2', img: '/bouquet.jpeg' },
-        { id: 3, productName: 'Товар 3', img: '/bouquet.jpeg' },
-        { id: 4, productName: 'Товар 4', img: '/bouquet.jpeg' },
-        { id: 5, productName: 'Товар 5', img: '/bouquet.jpeg' },
-        { id: 6, productName: 'Товар 6', img: '/bouquet.jpeg' },
-        { id: 7, productName: 'Товар 7', img: '/bouquet.jpeg' },
-        { id: 8, productName: 'Товар 8', img: '/bouquet.jpeg' },
-        { id: 9, productName: 'Товар 9', img: '/bouquet.jpeg' },
-        { id: 10, productName: 'Товар 10', img: '/bouquet.jpeg' }
+        { id: 1, productName: 'Букет 1', img: '/bouquet.jpeg' },
+        { id: 2, productName: 'Букет 2', img: '/bouquet.jpeg' },
+        { id: 3, productName: 'Букет 3', img: '/bouquet.jpeg' },
+        { id: 4, productName: 'Букет 4', img: '/bouquet.jpeg' },
+        { id: 5, productName: 'Букет 5', img: '/bouquet.jpeg' },
+        { id: 6, productName: 'Букет 6', img: '/bouquet.jpeg' },
+        { id: 7, productName: 'Букет 7', img: '/bouquet.jpeg' },
+        { id: 8, productName: 'Букет 8', img: '/bouquet.jpeg' },
+        { id: 9, productName: 'Букет 9', img: '/bouquet.jpeg' },
+        { id: 10, productName: 'Букет 10', img: '/bouquet.jpeg' }
       ]
     }
     return new Response(JSON.stringify(products), {
