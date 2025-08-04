098e797223d37e2a8c4cf71b1fc03f4791d8fcab
OlgaBorzdyko <helga.borzdyko@gmail.com>
2025-08-04 14:57:51 +0400

    fix for footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,19 +1,7 @@
-import styled from 'styled-components'
+import { Box } from '@mui/material'
 
 const Footer = () => {
-  return <Wrapper>Footer</Wrapper>
+  return <Box>Footer</Box>
 }
 
 export default Footer
-
-const Wrapper = styled.div`
-  background-color: rgb(29, 12, 39);
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: center;
-  position: relative;
-  height: auto;
-  max-height: 300px;
-  padding: 1rem;
-`
