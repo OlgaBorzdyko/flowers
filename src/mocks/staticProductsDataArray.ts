5b7734aa6d550b32b53f6c7fdf8bfe0deef479d1
OlgaBorzdyko <helga.borzdyko@gmail.com>
2025-08-05 13:44:54 +0400

    static array changed

--- src/mocks/staticProductsDataArray.ts
+++ src/mocks/staticProductsDataArray.ts
@@ -2,14 +2,15 @@ export const staticProductsDataArray = [
   {
     id: 1,
     categoryName: 'Букеты',
+    img: '/mono.jpeg',
     products: [
       { id: 1, productName: 'Букет № 1', img: '/bouquet.jpeg' },
       { id: 2, productName: 'Букет № 2', img: '/bouquet.jpeg' },
       { id: 3, productName: 'Букет № 3', img: '/bouquet.jpeg' }
     ]
   },
-  { id: 2, categoryName: 'Домашние растения' },
-  { id: 3, categoryName: 'Корзины' },
-  { id: 4, categoryName: 'Цветы' },
-  { id: 5, categoryName: 'Монобукеты' }
+  { id: 2, categoryName: 'Домашние растения', img: '/mono.jpeg' },
+  { id: 3, categoryName: 'Корзины', img: '/mono.jpeg' },
+  { id: 4, categoryName: 'Цветы', img: '/mono.jpeg' },
+  { id: 5, categoryName: 'Монобукеты', img: '/mono.jpeg' }
 ]
