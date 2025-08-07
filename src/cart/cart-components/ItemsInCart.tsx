5140db107405142e7491f004c75379a6546aedcf
OlgaBorzdyko <helga.borzdyko@gmail.com>
2025-08-07 15:44:35 +0400

    Items in cart changed with stack mui

--- src/cart/cart-components/ItemsInCart.tsx
+++ src/cart/cart-components/ItemsInCart.tsx
@@ -1,4 +1,4 @@
-import { Box, Card, Typography } from '@mui/material'
+import { Box, Card, Stack, Typography } from '@mui/material'
 
 import { useCart } from '../useCart'
 import QuantityChanging from './QuantityChanging'
@@ -6,20 +6,20 @@ import QuantityChanging from './QuantityChanging'
 const ItemsInCart = () => {
   const { items } = useCart()
   return (
-    <Box display="flex" flexDirection="column">
+    <>
       {items.map((item, index) => (
-        <Box key={index}>
-          <Card
-            sx={{
-              backgroundColor: 'background.default',
-              display: 'flex',
-              justifyContent: 'space-between',
-              alignItems: 'center',
-              flexDirection: 'row',
-              width: '100%',
-              gap: 2,
-              padding: 2
-            }}
+        <Card
+          key={index}
+          sx={{
+            backgroundColor: 'background.default',
+            padding: 2
+          }}
+        >
+          <Stack
+            alignItems="center"
+            direction="row"
+            justifyContent="space-between"
+            spacing={2}
           >
             <img
               alt={item.productName}
@@ -36,10 +36,10 @@ const ItemsInCart = () => {
             <Box>
               <QuantityChanging item={item} itemQuantity={item.quantity} />
             </Box>
-          </Card>
-        </Box>
+          </Stack>
+        </Card>
       ))}
-    </Box>
+    </>
   )
 }
 
