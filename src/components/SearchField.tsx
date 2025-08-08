875a7269409df976955724b479919b729768468e
OlgaBorzdyko <helga.borzdyko@gmail.com>
2025-08-08 19:18:09 +0400

    Added searc field func

--- src/components/SearchField.tsx
+++ src/components/SearchField.tsx
@@ -1,9 +1,31 @@
-import { Box, TextField } from '@mui/material'
+import { Box, List, TextField, Typography } from '@mui/material'
+import { useState } from 'react'
+
+import { useCart } from '../cart/useCart'
 
 const SearchField = () => {
+  const [inputValue, setInputValue] = useState('')
+  const { allProducts } = useCart()
+  const onHandleChange = (e) => {
+    setInputValue(e.target.value)
+  }
+  const filtered =
+    inputValue.length >= 1
+      ? allProducts.filter((product) =>
+          product.productName.toLowerCase().includes(inputValue.toLowerCase())
+        )
+      : []
   return (
     <Box>
-      <TextField />
+      <TextField onChange={onHandleChange} />
+
+      {inputValue.length >= 1 && filtered.length === 0 && (
+        <Typography>Совпадения не найдены</Typography>
+      )}
+
+      {filtered.map((product) => (
+        <List key={product.id}>{product.productName}</List>
+      ))}
     </Box>
   )
 }
